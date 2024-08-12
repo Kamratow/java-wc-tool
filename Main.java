8b1e9d3d4d2068437992c0f39e6c2de00816f224
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-12 12:56:45 +0200

    Created some simple to get user input that can be used by ccwctool

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String a[]) {
         Ccwctool ccwctool = new Ccwctool();
 
-        ccwctool.sayHi();
+        ccwctool.startToool();
     }
 }
--- com/kamratow/ccwctool/Ccwctool.java
+++ com/kamratow/ccwctool/Ccwctool.java
@@ -1,7 +1,21 @@
 package com.kamratow.ccwctool;
 
+import java.util.Scanner;
+
 public class Ccwctool {
-    public void sayHi() {
-        System.out.println("Hi, this is ccwctool!");
+    public void startToool() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ccwc tool started, please provide command to run");
+
+        String userInput = scanner.nextLine();
+
+        if (userInput.startsWith("ccwc")) {
+            System.out.println("ccwc command running...");
+        } else {
+            System.out.println("Unable to run provided command - please check your input and try again");
+        }
+
+        scanner.close();
     }
 }
